bd80f8eadec19f4fd986c1a1df34bae730002fd2
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-10-02 23:41:51 +0300

    ECommerceApp

---
+++ API/Elasticsearch/Elasticsearch.WEB/Controllers/ECommerceController.cs
@@ -0,0 +1,26 @@
+﻿using Elasticsearch.WEB.Services;
+using Elasticsearch.WEB.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elasticsearch.WEB.Controllers;
+
+public class ECommerceController : Controller
+{
+    private readonly ECommerceService _service;
+
+    public ECommerceController(ECommerceService service)
+    {
+        _service = service;
+    }
+
+    public async Task<IActionResult> Search([FromQuery]SearchPageViewModel searchViewModel)
+    {
+        var (eCommerceList, totalCount, pageLinkCount) = await _service.SearchAsync(searchViewModel.ECommerceSearchViewModel, searchViewModel.Page, searchViewModel.PageSize);
+
+        searchViewModel.List = eCommerceList;
+        searchViewModel.TotalCount = totalCount;
+        searchViewModel.PageLinkCount = pageLinkCount;
+
+        return View(searchViewModel);
+    }
+}
